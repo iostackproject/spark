63022cd44269b7c41378cce3ee20118c62bcfb1a
genis ortiz <genis.ortiz.gomis@gmail.com>
2015-07-07 12:06:46 +0200

    Added verification on compile script

--- compile.sh
+++ compile.sh
@@ -3,18 +3,21 @@
 # Usage:
 # ./compile
 #			make all modules and packaging all project to spark-2.6.0-bin-spark-swift.tgz
-# ./compile <any words>
-#			make only swift module and packaging all project to spark-2.6.0-bin-spark-swift.tgz
-#
-#
-# To build more modules, add a comma separated list of module names behind the -pl argument
-# Add custom parameters to mvn at the end
-# Recap:			./make-distribution.sh --name <build-name> --tgz <mvn arguments>
-#							<mvn arguments>			-Phadoop-2.6 -Pyarn -pl swift,core,graphx
+# ./compile <module name>
+#			make only the module named and packaging all project to spark-2.6.0-bin-spark-swift.tgz
+# ./compile <comma separated list of module names>
+#			make all named modules and packaging all project to spark-2.6.0-bin-spark-swift.tgz
 
 if [ $# -ne 0 ]
 then
-	./make-distribution.sh --name spark-swift --tgz -Phadoop-2.6 -pl examples
+	./make-distribution.sh --name spark-swift --tgz -Phadoop-2.6 -pl $@
 else
-	./make-distribution.sh --name spark-swift --tgz -Phadoop-2.6
+	while true; do
+	    read -p "Do you really wish to compile all modules of Spark (it can take more than one hour)? [y,n] " yn
+	    case $yn in
+	        [Yy]* ) ./make-distribution.sh --name spark-swift --tgz -Phadoop-2.6; break;;
+	        [Nn]* ) exit;;
+	        * ) echo "Please answer yes or no.";;
+	    esac
+	done
 fi
